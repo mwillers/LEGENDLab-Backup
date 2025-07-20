ca93de09071a4b450348f78f1fc3de4d5d851728
Michael Willers <mwillers@ph.tum.de>
2025-07-20 09:21:51 +0200

    Fix errors in backup.sh

--- backup.sh
+++ backup.sh
@@ -13,13 +13,13 @@ exec 2>&1
 echo "###### Starting backup on $(date) ######"
 
 # Create list of installed software
-dpkg --get-selections > "${BACKUP_LOG_DIR}/software.list
+dpkg --get-selections > "${BACKUP_LOG_DIR}/software.list"
 
-## Create list of installed vscode extensions
+# Create list of installed vscode extensions
 jq -r '.[] | .identifier.id' ~/.vscode-server/extensions/extensions.json > "${BACKUP_LOG_DIR}/vscode-server-extensions.list"
-code --list-extensions > "${BACKUP_LOG_DIR}/vscode-extensions.list
+code --list-extensions > "${BACKUP_LOG_DIR}/vscode-extensions.list"
 
-## Sync backup data
+# Sync backup data
 echo "Syncing backup files ..."
 borg create -v --stats                                  \
     $REPOSITORY::'{now:%Y%m%d-%H%M}'                    \
